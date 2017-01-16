9d9f668e0a803e4f3e68a8f95a21bf1107ed6f66
szhou1 <zhousteve3@gmail.com>
2017-01-15 20:31:12 -0800

    Consider options for 'Other' fund

--- distrCalc.js
+++ distrCalc.js
@@ -14,7 +14,6 @@ orders.forEach(function(order) {
     fees.forEach(function(feeCatType) {
       if(feeCatType.order_item_type === item.type) {
         
-        console.log(feeCatType.fees[0].amount);
         var price = feeCatType.fees[0].amount;
         
         feeCatType.distributions.forEach(function(distr) {
@@ -22,12 +21,18 @@ orders.forEach(function(order) {
           price -= amount;
           distributions[distr.name] = amount;
 
-          if(totalDistributions[distr.name]) {
-            totalDistributions[distr.name] = totalDistributions[distr.name] + amount;
-          } else {
-            totalDistributions[distr.name] = amount;
-          }
+          totalDistributions[distr.name] 
+            ? totalDistributions[distr.name] = totalDistributions[distr.name] + amount
+            : totalDistributions[distr.name] = amount
+
         });
+        if(price > 0) {
+          distributions['Other'] = price;
+
+          totalDistributions['Other'] 
+            ? totalDistributions['Other'] = totalDistributions['Other'] + price
+            : totalDistributions['Other'] = price
+        }
 
         console.log(distributions)
       }
