b7fd7dbabca064dacefc12de98179fd6f0191fc9
szhou1 <zhousteve3@gmail.com>
2017-01-16 10:09:49 -0800

    Add more tests

--- part3/test/calc.js
+++ part3/test/calc.js
@@ -18,6 +18,85 @@ describe('Calc', () => {
         }).catch((e) => {
         });
     });
+
+    it('should return fees for an order', () => {
+      chai.request(server)
+        .post('/order/fees')
+        .send(
+            [
+              {
+                "order_date": "1/11/2015",
+                "order_number": "20150111000001",
+                "order_items": [
+                  {
+                    "order_item_id": 1,
+                    "type": "Real Property Recording",
+                    "pages": 30
+                  },
+                  {
+                    "order_item_id": 2,
+                    "type": "Real Property Recording",
+                    "pages": 1
+                  }
+                ]
+              }
+            ]
+          )
+        .then((res) => {
+          expect(res).to.have.status(200);
+          expect(res.body).to.be.a('array');
+          expect(res.body.length).to.equal(1);
+          expect(res.body[0].order_items.length).to.equal(2);
+        });
+    });
+
   });
+
+  describe('#getDistributions()', () => {
+    it('should return 400 status code if no body is passed in', () => {
+      chai.request(server)
+        .post('/order/distr')
+        .then((res) => {
+          expect(res).to.have.status(400);
+        }).catch((e) => {
+        });
+    });
+
+    it('should return distributions for an order', () => {
+      chai.request(server)
+        .post('/order/distr')
+        .send(
+            [
+              {
+                "order_date": "1/11/2015",
+                "order_number": "20150111000001",
+                "order_items": [
+                  {
+                    "order_item_id": 1,
+                    "type": "Real Property Recording",
+                    "pages": 30
+                  },
+                  {
+                    "order_item_id": 2,
+                    "type": "Real Property Recording",
+                    "pages": 1
+                  }
+                ]
+              }
+            ]
+          )
+        .then((res) => {
+          expect(res).to.have.status(200);
+          expect(res.body).to.be.a('object');
+          expect(res.body).to.have.all.keys(['orders', 'totalDistributions']);
+          expect(res.body.orders.length).to.equal(1);
+          expect(res.body.orders[0].distributions.length).to.equal(4);
+          expect(res.body.totalDistributions.length).to.equal(5);
+        });
+    });
+
+  });
+
+
 
 });
